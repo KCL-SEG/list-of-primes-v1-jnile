a7b9d70c61c2352a818fd39df82e940f5318490f
Jaidev Nileshkumar <jaidev.nileshkumar@kcl.ac.uk>
2022-09-25 12:14:54 +0100

    Add method to get list of primes

--- primes.py
+++ primes.py
@@ -3,4 +3,21 @@
 
 def primes(number_of_primes):
     list = []
+
+    currentNum = 2
+
+    while len(list) < number_of_primes:
+        #check if the current number is a multiple of any prime we already have
+        multipleOfNum = False
+        for p in list:
+            if (currentNum % p) == 0 :
+                multipleOfNum = True
+                break
+        #Not a multiple of a number // hence prime
+        if not multipleOfNum:
+            list.append(currentNum)
+
+        #Increment current number
+        currentNum += 1
+
     return list
